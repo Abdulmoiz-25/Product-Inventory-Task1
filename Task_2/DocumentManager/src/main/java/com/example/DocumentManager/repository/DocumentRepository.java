f01c5d6ae9b20f95a518882c67a6265ac888c297
Abdul Moiz Meer <abdulmoizmeer230@gmail.com>
2025-02-25 12:45:10 +0500

    udatae

--- Task_2/DocumentManager/src/main/java/com/example/DocumentManager/repository/DocumentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.DocumentManager.repository;
-
-import com.example.DocumentManager.model.Document;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DocumentRepository extends JpaRepository<Document, Long> {
-}
